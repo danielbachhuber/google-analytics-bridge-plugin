35e4275f31020c579af879d3989ae6ea251ec8db
Daniel Bachhuber <daniel@bachhuber.co>
2018-10-02 11:23:10 -0700

    Initial commit

---
+++ google-analytics-bridge.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Plugin Name:     Google Analytics Bridge
+ * Plugin URI:      https://handbuilt.co
+ * Description:     API bridge between WordPress and Google Analytics
+ * Author:          Daniel Bachhuber
+ * Author URI:      https://handbuilt.co
+ * Text Domain:     google-analytics-bridge
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Google_Analytics_Bridge
+ */
+
+// Your code starts here.
+
+
+/**
+ * Register the class autoloader
+ */
+spl_autoload_register( function( $class ) {
+	$class = ltrim( $class, '\\' );
+	if ( 0 !== stripos( $class, 'GAB\\' ) ) {
+		return;
+	}
+
+	$parts = explode( '\\', $class );
+	array_shift( $parts ); // Don't need "GAB"
+	$last = array_pop( $parts ); // File should be 'class-[...].php'
+	$last = 'class-' . $last . '.php';
+	$parts[] = $last;
+	$file = dirname( __FILE__ ) . '/inc/' . str_replace( '_', '-', strtolower( implode( $parts, '/' ) ) );
+	if ( file_exists( $file ) ) {
+		require $file;
+	}
+
+});
